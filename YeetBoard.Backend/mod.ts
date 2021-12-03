edf4ab70c21fcf8ee44e10aedf3a56b883e8744f
Schufeli <me@schufi.codes>
2021-12-03 12:51:21 +0100

    Added: basic controller for application

--- YeetBoard.Backend/mod.ts
+++ YeetBoard.Backend/mod.ts
@@ -1,13 +1,18 @@
-import { _application } from './deps.ts';
+import { _application, _context, _router } from './deps.ts';
 import { loadConfig } from './lib/classes/config.ts';
+import { getAllCards, getCardById, createCard } from './controllers.ts';
 
 const app = new _application();
+const router = new _router();
 const config = await loadConfig('./config.yml');
 
+router.get('/', (context: _context) => {
+    context.response.body = 'Load Frontend here!'; // TODO: load frontend project here
+})
+    .get('/cards', getAllCards)
+    .get('/cards:id', getCardById)
+    .post('/cards', createCard)
 
-
-app.use(() => { // TODO: remove after development
-    console.log("Server up and running..."); 
-});
-
+app.use(router.routes());
+app.use(router.allowedMethods());
 await app.listen({ port: config.port });
