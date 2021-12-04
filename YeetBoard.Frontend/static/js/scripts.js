688394217b9d2b42c1953f5e5e0ede6450ab9d9d
Robin Sigrist <sigrist@simpit.ch>
2021-12-04 18:38:43 +0100

    - Some changes for layout - Some added buttons for delete and move - added Bootstrap

---
+++ YeetBoard.Frontend/static/js/scripts.js
@@ -0,0 +1,15 @@
+(function() {
+    // fetch('/cards')
+    //     // .then(response => response.json())
+    //     .then(
+    //         data => console.log(data)
+    //         data => cons    ole.log(data)
+    // );
+    fetch('/cards')
+    .then(function() {
+        data => console.log(data)
+    })
+    .catch(function() {
+
+    });
+})
